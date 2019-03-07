4916851d74aacd389dfc480c6cd70c35b8ab1b39
Daniele Borra <daniele@adoratorio.com>
2019-03-07 15:32:43 +0100

    First commit

---
+++ Footer/CompanyDetails/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="c-element c-element--company-details">
+    <!-- Company details HTML -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CompanyDetails',
+}
+</script>
+
+<style lang="scss" scoped>
+.c-element.c-element--company-details {
+  
+}
+</style>
---
+++ Footer/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer class="c-template c-template--footer">
+    <!-- Footer template slots -->
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'FooterTemplate',
+}
+</script>
+
+<style lang="scss" scoped>
+.c-template.c-template--footer {
+  
+}
+</style>
