f6d1c6311b9bfdbd91e1acf609c379e452b189af
Andrea <Andrea.Gottardi.1993@gmail.com>
2019-03-26 15:32:37 +0100

    Add VideoTemplate.vue & Plyr

---
+++ Video/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div
+    class="c-template c-template--video"
+  >
+    <slot
+      v-for="slot in slots"
+      :name="slot"
+    />
+  </div>
+</template>
+
+<script>
+import Plyr from 'plyr';
+
+export default {
+  name: 'VideoComponent',
+
+  props: {
+    theme: {
+      type: String,
+      required: false,
+      default: 'default',
+    },
+    slots: {
+      type: Array,
+      required: false,
+      default() {
+        return ['player', 'controls'];
+      },
+    },
+  },
+
+  mounted() {
+    console.log(Plyr);
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import '~sass-mq/mq';
+</style>
