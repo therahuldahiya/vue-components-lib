8d7c69024b565d583ca3f338859d5a5b26b89e4e
Daniele Borra <daniele@adoratorio.com>
2019-03-27 17:50:53 +0100

    Added swipe and pan with multiple param

--- Slider/SliderCore/index.vue
+++ Slider/SliderCore/index.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="slider__core">
-    <div class="slider__wrapper">
+    <div
+      v-if="!multiple"
+      class="slider__wrapper"
+    >
       <transition
         :css="css.use"
         :name="css.animation"
@@ -10,10 +13,23 @@
       >
         <div
           :key="currentSlide"
+          ref="slide"
           :style="{ background: slides[currentSlide].color }"
           class="slide"
         />
       </transition>
+    </div>
+    <div
+      v-else
+      class="slider__wrapper"
+    >
+      <div
+        v-for="(slide, index) in slides"
+        :key="index"
+        :style="{ background: slide.color, zIndex: slides.length - index }"
+        class="slide"
+        :data-active="index === currentSlide ? true : false"
+      />
     </div>
     <Controls :current-slide="currentSlide" />
     <Dots :slides="slides" />
@@ -25,6 +41,7 @@
 </template>
 
 <script>
+import Hammer from 'hammerjs';
 import Controls from './Controls';
 import Dots from './Dots';
 import Counter from './Counter';
@@ -53,6 +70,11 @@ export default {
         };
       },
     },
+    multiple: {
+      type: Boolean,
+      required: false,
+      default: true,
+    },
     mode: {
       type: String,
       required: false,
@@ -74,6 +96,7 @@ export default {
     return {
       currentSlide: 0,
       direction: 0,
+      hm: null,
     };
   },
 
@@ -81,6 +104,26 @@ export default {
     this.$on('slider-prev', this.prev);
     this.$on('slider-next', this.next);
     this.$on('slider-dot', this.dot);
+
+    this.hm = new Hammer.Manager(this.$el);
+    if (!this.multiple) {
+      const swipe = new Hammer.Swipe();
+      this.hm.add(swipe);
+      this.hm.on('swiperight', this.prev);
+      this.hm.on('swipeleft', this.next);
+    } else {
+      const pan = new Hammer.Pan();
+      this.hm.add(pan);
+      this.hm.on('panstart', this.panStart);
+      this.hm.on('panmove', this.panMove);
+      this.hm.on('panend pancancel', this.panEnd);
+    }
+  },
+
+  beforeDestroy() {
+    this.hm.off('swiperight');
+    this.hm.off('swipeleft');
+    this.hm = null;
   },
 
   methods: {
@@ -98,6 +141,20 @@ export default {
       this.direction = event.index > this.currentSlide ? 1 : -1;
       this.currentSlide = event.index;
     },
+    panMove(event) {
+      if (!this.multiple) {
+        this.$refs.slide.style.transform = `translateX(${event.deltaX}px)`;
+      } else {
+        this.$el.querySelector('[data-active="true"]').style.transform = `translateX(${event.deltaX}px)`;
+      }
+    },
+    panEnd() {
+      if (!this.multiple) {
+        this.$refs.slide.style.transform = `translateX(${0}px)`;
+      } else {
+        this.$el.querySelector('[data-active="true"]').style.transform = `translateX(${0}px)`;
+      }
+    },
   },
 };
 </script>
@@ -112,6 +169,7 @@ export default {
     width: 400px;
     height: 400px;
     margin: 0 auto;
+    overflow: hidden;
   }
 
   .slide {
